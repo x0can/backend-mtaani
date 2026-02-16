154a2ee037d6b0537a319267a9cb7c367fe500c1
x0can <alexmwaura43@gmail.com>
2026-02-16 21:05:15 +0300

    update password change route v3

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const router = express.Router();
 
+const bcrypt = require("bcryptjs")
+
 const { User, Order } = require("../db");
 const { authMiddleware, adminOnly } = require("../auth");
 
